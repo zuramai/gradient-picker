e4166a825646cef493fd671ab193a45f191fcd05
Fauzan <fncolon@pm.me>
2023-01-14 21:08:19 +0700

    feat: color handlers should defined in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import './style.css'
 
 const app = document.getElementById('app')!
 const preview = document.getElementById('preview')!
+const colorHandlers = document.createElement('div')
+colorHandlers.classList.add('color__handlers')
+
+// TODO: publish UI library
 new GradientPicker({
   el: app,
-  previewEl: preview
+  previewEl: preview,
+  colorHandlersEl: colorHandlers,
 })
