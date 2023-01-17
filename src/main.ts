3136b94ebd882c933d8898f3ca39e35a6479a080
zuramai <saugi.dev@gmail.com>
2023-01-17 07:25:06 +0700

    feat: copy textarea functionality

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,19 @@ const colorHandlers = document.createElement('div')
 colorHandlers.classList.add('color__handlers')
 
 // TODO: publish UI library
-new GradientPicker({
+const picker = new GradientPicker({
   el: app,
   previewEl: preview,
   colorHandlersEl: colorHandlers,
+})
+
+const copyCssButton = document.getElementById('copy-css')
+copyCssButton?.addEventListener('click', () => {
+  let textarea = document.querySelector("#css") as HTMLTextAreaElement;
+  navigator.clipboard.writeText(textarea.value);
+  
+  // Show "Copied!" text
+  const copiedEl = document.getElementById('copied')
+  copiedEl?.classList.add('show')
+  setTimeout(() => copiedEl?.classList.remove('show'), 500);
 })
